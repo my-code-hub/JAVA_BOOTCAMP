b8ba96649a61eb57fa0a1b0df8c16439eff62c03
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-11-30 09:58:20 +0200

    less 3 solutions

---
+++ src/lv/rcs/lesson3/ConditionalMath.java
@@ -0,0 +1,24 @@
+package lv.rcs.lesson3;
+
+import java.util.Scanner;
+
+public class ConditionalMath {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("please enter integer:");
+        int a = scanner.nextInt();
+
+        System.out.println("please enter float:");
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println(a + " is greater than " + b);
+        } else if (b > a) {
+            System.out.println(b + " is greater than " + a);
+        } else {
+            System.out.println(a + " is equal to " + b);
+        }
+    }
+}
---
+++ src/lv/rcs/lesson3/GreetingsInteractive.java
@@ -0,0 +1,18 @@
+package lv.rcs.lesson3;
+
+import java.util.Scanner;
+
+public class GreetingsInteractive {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("please enter your name:");
+        String name = scanner.next();
+
+        System.out.println("please enter your name:");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello, my name is " + name + " I am " + age + " years old");
+    }
+}
