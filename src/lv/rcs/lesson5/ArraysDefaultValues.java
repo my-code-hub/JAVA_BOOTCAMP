90d81d78eb5c9ab6246fa689842d9e035f7ed6f5
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-02 19:59:55 +0200

    lesson 5 examples

---
+++ src/lv/rcs/lesson5/ArraysDefaultValues.java
@@ -0,0 +1,27 @@
+package lv.rcs.lesson5;
+
+public class ArraysDefaultValues {
+
+    public static void main(String[] args) {
+        // default values:
+        System.out.println("\n\nInteger array default values:");
+        int num[] = new int[5];
+        for (int val : num)
+            System.out.print(val + " ");
+
+        System.out.println("\n\nDouble array default values:");
+        double dnum[] = new double[5];
+        for (double val : dnum)
+            System.out.print(val + " ");
+
+        System.out.println("\n\nBoolean array default values:");
+        boolean bnum[] = new boolean[5];
+        for (boolean val : bnum)
+            System.out.print(val + " ");
+
+        System.out.println("\n\nChar array default values:");
+        char chararr[] = new char[5];
+        for (char val : chararr)
+            System.out.print(val + " ");
+    }
+}
