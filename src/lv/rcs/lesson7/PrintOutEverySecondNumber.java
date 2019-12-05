baa219fed2217e649d00d7824543528290d590d5
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-05 23:54:17 +0200

    print out every second number example

---
+++ src/lv/rcs/lesson7/PrintOutEverySecondNumber.java
@@ -0,0 +1,10 @@
+package lv.rcs.lesson7;
+
+public class PrintOutEverySecondNumber {
+
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i = i - 2) {
+            System.out.println(i);
+        }
+    }
+}
