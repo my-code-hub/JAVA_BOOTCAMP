eb0332226024ccb2c7d661b57455ac092c24fa02
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-05 21:16:37 +0200

    lesson 7 - task 4

---
+++ src/lv/rcs/lesson7/Task4.java
@@ -0,0 +1,23 @@
+package lv.rcs.lesson7;
+
+public class Task4 {
+
+//    4. declare and initialize array with four arbitrary whole numbers.
+//    Print out all of the numbers from array.
+    public static void main(String[] args) {
+
+        int[] numbers = {8, 2, 5, 7}; // declare and initialize
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            System.out.println("i value is: " + i);
+            System.out.println("numbers[" + i + "] is: " + numbers[i]);
+        }
+
+        // solution without loop
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+
+    }
+}
