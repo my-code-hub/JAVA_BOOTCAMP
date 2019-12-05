632c1c8df091858b943d64396de047b5dbd212b3
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-05 19:56:53 +0200

    Exercise 2 solution

--- src/lv/rcs/lesson7/Task2.java
+++ src/lv/rcs/lesson7/Task2.java
@@ -2,10 +2,14 @@ package lv.rcs.lesson7;
 
 public class Task2 {
 
-    //1.2 print out all numbers from 13 to -3 using for loop
+    // 2.0 lets print out sum of all numbers
+    // between 3 and 12 (use for loop)
     public static void main(String[] args) {
-        for (int i = 13; i >= -3; i--) {
-            System.out.print(i + " ");
+        int sum = 0;
+        for (int i = 3; i <= 12; i++) {
+            //sum = sum + i;
+            sum += i;
         }
+        System.out.println(sum);
     }
 }
