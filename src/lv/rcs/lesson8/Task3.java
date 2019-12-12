98fb9091fb0a1f49bd404b7bc86850cd39fb7251
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-12 21:02:44 +0200

    task 3 & lesson 8

---
+++ src/lv/rcs/lesson8/Task3.java
@@ -0,0 +1,13 @@
+package lv.rcs.lesson8;
+
+public class Task3 {
+
+    public static void main(String [] args) {
+        multiply(5, 5); //25
+        multiply(2, 2); //4
+    }
+
+    static void multiply(int x1, int x2) {
+        System.out.println(x1 * x2);
+    }
+}
