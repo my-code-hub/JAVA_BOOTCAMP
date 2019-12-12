52afea7137aa99dca141a3e847cc8c9c719d2a81
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-12 20:20:33 +0200

    lesson 8 - solutions for task 1 and task 2

---
+++ src/lv/rcs/lesson8/Task1.java
@@ -0,0 +1,13 @@
+package lv.rcs.lesson8;
+
+public class Task1 {
+
+    public static void main(String [] args) {
+        outputText();
+    }
+
+    static void outputText() {
+        System.out.println("Hello, John!");
+    }
+}
+
