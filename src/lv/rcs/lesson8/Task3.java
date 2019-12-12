78094058c1aa7e8a394bc71eb7df12b1b2bbe4c4
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-12 21:20:13 +0200

    Task 3 & lesson 8

--- src/lv/rcs/lesson8/Task3.java
+++ src/lv/rcs/lesson8/Task3.java
@@ -1,10 +1,18 @@
 package lv.rcs.lesson8;
 
+import java.util.Scanner;
+
 public class Task3 {
 
     public static void main(String [] args) {
         multiply(5, 5); //25
         multiply(2, 2); //4
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter two whole numbers");
+        int skaitlis1 = scanner.nextInt();
+        int skaitlis2 = scanner.nextInt();
+        multiply(skaitlis1, skaitlis2);
     }
 
     static void multiply(int x1, int x2) {
