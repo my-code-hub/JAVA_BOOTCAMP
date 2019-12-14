d0f78e1f9c5e23a324e0f92366fd7a74cbe510ed
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-14 11:12:20 +0200

    task6 & lesson 8/9

---
+++ src/lv/rcs/lesson8/Task6.java
@@ -0,0 +1,25 @@
+package lv.rcs.lesson8;
+
+//6. Implement a method "isPositive" which has one parameter
+//   (whole number) and returns true if it is
+//   positive or zero and false, if it is negative.
+//   Write a program which uses this method and outputs
+//   the result to user.
+public class Task6 {
+
+    public static void main(String[] args) {
+        System.out.println(isPositive(-15));
+
+        int number = 20;
+        boolean result = isPositive(number);
+        System.out.println("20 is positive?: " + result);
+    }
+
+    static boolean isPositive(int number) {
+        if (number >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
