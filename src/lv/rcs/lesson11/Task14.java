93c7bbb4e52b6bc9dcb6ca7cdf307c460080f6fb
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-17 19:45:34 +0200

    rename to task 14

---
+++ src/lv/rcs/lesson11/Task14.java
@@ -0,0 +1,22 @@
+package lv.rcs.lesson11;
+
+public class Task14 {
+
+    //14. Implement a method "calculateAverage" which has one parameter (array of whole numbers) calculates
+    //    and returns the average of all number in array. Write an example program which uses this
+    //    method and prints out the result.
+    //    Example of method usage:
+    //        int [] numbers = {1, 2, 3};
+    //        int result = calculateAverage(numbers);
+    //        System.out.println(result); //prints out 2
+    //    hint: To calculate the average value of all the numbers in array you will need to sum all
+    //          numbers in array and divide it by the length of array.
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3};
+
+        //1) uzrakstam ciklu, kas izvada visus skaitļus šajā masīva
+        //2) sasummēt visus skaitļus šajā masīvā
+        //3) izdalīt šo skaitļu summu ar masīva izmēru (numbers.length)
+        //4) izvadīt rezultātu lietotājam
+    }
+}
