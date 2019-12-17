eaeb81cea84e56cdc6060f3bd188817f0d37a549
Ritvars Dortans <ritvars.dortans@neotech.lv>
2019-12-17 19:44:41 +0200

    task 13 - part 1

---
+++ src/lv/rcs/lesson11/Task13.java
@@ -0,0 +1,13 @@
+package lv.rcs.lesson11;
+
+public class Task13 {
+
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3};
+
+        //1) uzrakstam ciklu, kas izvada visus skaitļus šajā masīva
+        //2) sasummēt visus skaitļus šajā masīvā
+        //3) izdalīt šo skaitļu summu ar masīva izmēru (numbers.length)
+        //4) izvadīt rezultātu lietotājam
+    }
+}
