019543f913669f7e43d03c1dfa7c6460e7bc0dcb
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-01-09 20:51:50 +0200

    exercise 15.2 solution

--- src/lv/rcs/lesson15/excersises/p1/B.java
+++ src/lv/rcs/lesson15/excersises/p1/B.java
@@ -2,9 +2,9 @@ package lv.rcs.lesson15.excersises.p1;
 
 // Change access modifier for this class so it would be accessible only inside of this package.
 // (so we can only create object of type B in InnerApp class, but not BaseApp class)
-public class B {
+class B {
 
-    public B() {
+    B() {
         System.out.println("class B() constructor called!");
     }
 }
--- src/lv/rcs/lesson15/excersises/p1/InnerApp.java
+++ src/lv/rcs/lesson15/excersises/p1/InnerApp.java
@@ -5,32 +5,44 @@ public class InnerApp {
     public static void main(String[] args) {
 
         //1. create new instance of class A using default constructor
+        A object = new A();
 
         //2. output the value of property "a" via the getter method
         //Note: if you did everything right, then the code below shouldn't be compiling:
-        //A object = new A();
-        //System.out.println(object.a);
+//        A object2 = new A();
+        System.out.println(object.getA());
 
         //3. output the value of property "b" via the getter method
         //Note: if you did everything right, then the code below shouldn't be compiling:
         // A object2 = new A();
         // System.out.println(object2.b);
+        System.out.println(object.getB());
 
         //4. output the value of property c
+        System.out.println(object.getC());
         //modify the value of property c to "Updated C!"
+        object.setC("Updated C!");
         //output the value of property c
+        System.out.println(object.getC());
 
         //5. output property value "d"
+        System.out.println(object.d);
         // update property "d" to value "InnerApp"
+        object.d = "InnerApp";
         // output property value "d"
+        System.out.println(object.d);
 
         //6. call method "local()"
+        object.local();
 
         //7. call method "famous()"
+        object.famous();
 
         //8. can you call method "secret" on this object? Why not?
+        //because it's private
 
         //9.create new instance of class B using default constructor
         // why can you do it in this class, but couldn't do it in class "BaseApp"?
+        B b = new B();
     }
 }
