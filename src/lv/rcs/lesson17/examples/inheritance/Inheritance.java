6e864c2466032b99ac0e0924053e7347d2bd613c
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-01-11 11:35:00 +0200

    inheritance examples

--- src/lv/rcs/lesson17/examples/inheritance/Inheritance.java
+++ src/lv/rcs/lesson17/examples/inheritance/Inheritance.java
@@ -7,21 +7,21 @@ public class Inheritance {
         // https://www.geeksforgeeks.org/inheritance-in-java/
         // inheriting properties and methods (public, protected and package (only if in the same package))
 
-        Sport sport1 = new Sport();
-        sport1.setName("ice hockey");
+        Sport sport1 = new Sport("Ice Hockey");
+        //sport1.setName("ice hockey");
         System.out.println(sport1.getName());
         sport1.description();
 
-        Running running = new Running();
-        running.setName("Running");
+        Running running = new Running("Running");
+//        running.setName("Running");
         running.setDistance(10);
         running.description();
 
 
 
+
+
         // constructor hierarchy
-        // overriding methods
-        // protected, package and public are available. private ones are accessible though
 
         //discuss Object and how it's a superclass of all objects (talk about toString())
         //any object actually inherits methods from Object
