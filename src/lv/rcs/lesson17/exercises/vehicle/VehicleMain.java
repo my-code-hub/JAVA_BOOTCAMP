7b1b48ae0ac34d2f7de370b0057c4f176d543631
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-01-11 11:45:05 +0200

    Vehicle main

---
+++ src/lv/rcs/lesson17/exercises/vehicle/VehicleMain.java
@@ -0,0 +1,44 @@
+package lv.rcs.lesson17.exercises.vehicle;
+
+// 17.1 Inheritance exercise:
+public class VehicleMain {
+
+    public static void main(String[] args) {
+        //Create object of type Vehicle with brand "none" and speed 5
+
+        //output the brand of this object
+        //call method move and description on this instance
+
+
+        //Create object of type Car with brand "BMW" and speed 90 and horsepower 200
+
+        //output the brand of this object
+        //call method description on this instance
+        //output fuel for this car
+        //add 50 fuel to this car
+        //output fuel for this car
+        //call method move
+        //call method "power" on this car
+
+
+        //Create object of type Car with brand "Porsche" and speed 200 and horsepower 400
+
+        //output the brand of this object
+        //call method description on this instance
+        //output fuel for this car
+        //add 50 fuel to this car
+        //output fuel for this car
+        //call method move
+        //call method "power" on this car
+
+
+        //Create object of type Bicycle with brand "Merida" and speed 30
+        //and seat height 5
+
+        //output seat height
+        //change seat height to 10
+        //output seat height
+        //call method move
+        //call method description
+    }
+}
