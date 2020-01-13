095ea17afe49f5d557a5d9f581ced799b06305ab
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-01-13 21:29:43 +0200

    other stuff

---
+++ src/lv/rcs/lesson18/examples/comparing/ComparingStrings.java
@@ -0,0 +1,29 @@
+package lv.rcs.lesson18.examples.comparing;
+
+public class ComparingStrings {
+
+    public static void main(String[] args) {
+        String name1 = "janis";
+        String name2 = "janis";
+
+        if (name1 == name2) {
+            System.out.println(name1 + " and " + name2 + " are equal!");
+        } else {
+            System.out.println(name1 + " and " + name2 + " are NOT equal!");
+        }
+
+        String name3 = new String("janis");
+        if (name1 == name3) {
+            System.out.println(name1 + " and " + name3 + " are equal!");
+        } else {
+            System.out.println(name1 + " and " + name3 + " are NOT equal!");
+        }
+
+        if (name1.equals(name3)) {
+            System.out.println(name1 + " and " + name3 + " are equal!");
+        } else {
+            System.out.println(name1 + " and " + name3 + " are NOT equal!");
+        }
+
+    }
+}
