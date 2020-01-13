75dd2c30b567e79cb5da9993cc2ea13803183db5
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-01-13 19:32:28 +0200

    lesson18 main topics

---
+++ src/lv/rcs/lesson18/examples/Lesson18.java
@@ -0,0 +1,11 @@
+package lv.rcs.lesson18.examples;
+
+public class Lesson18 {
+
+    public static void main(String[] args) {
+        //- static
+        //- comparing objects and strings // checking for null
+        //- working with String / Standard java API
+        //- multi dimensional arrays
+    }
+}
