b8794f60e8f02bc19fab1e3aca7531f1a3772e09
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-02-20 18:53:58 +0200

    assertions example

--- src/lv/rcs/bootcamp/exceptions/AssertionsExample.java
+++ src/lv/rcs/bootcamp/exceptions/AssertionsExample.java
@@ -3,6 +3,9 @@ package lv.rcs.bootcamp.exceptions;
 public class AssertionsExample {
 
     public static void main(String[] args) {
+        System.out.println("true: " + (5 != 5));
+        System.out.println("false: " + false);
+
         assert true;
         System.out.println("All good!");
 
@@ -11,5 +14,15 @@ public class AssertionsExample {
 
         assert x == y : "This assert failed because boolean is false";
         System.out.println("did I get here?");
+
+        divide(10, 0);
+    }
+
+    public static int divide(int a, int b) {
+
+        System.out.println("b == 0 " + (b == 0));
+
+        assert b != 0 : "b is 0";
+        return a / b;
     }
 }
